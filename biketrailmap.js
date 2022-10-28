d50ada234b563f6ddc63663612d8a176ae89282d
Richard Inman <91326618+richardneoky@users.noreply.github.com>
2022-10-27 22:01:47 -0400

    Add files via upload

--- biketrailmap.js
+++ biketrailmap.js
@@ -1,7 +1,7 @@
 const toggle = (type) => {
     const matches = document.getElementsByClassName(type)
     for (element of matches){
-      if (element.style.display = 'none'){
+      if (element.style.display === 'none'){
         element.style.display = "block"
       } else {
         element.style.display = "none"
